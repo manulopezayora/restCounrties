86a684d4e8778b70729a16c2230cc78efa0f9dc5
Manuel Jesús López <51477144+manulopezayora@users.noreply.github.com>
2022-10-20 19:04:20 +0200

    [DEV] Add service and baseUrl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './home/shared/header/header.component';
 import { SearchInputComponent } from './home/components/search-input/search-input.component';
 import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/services/restCountries/rest-countries.service.ts
@@ -0,0 +1,21 @@
+import { environment } from './../../../../environments/environment';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { RESTCountries } from '../../interfaces/RESTCountries.interface';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestCountriesService {
+  private baseUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.baseUrl = `${environment.baseUrl}${environment.version}`;
+  }
+
+  public getAllCountries(): Observable<RESTCountries[]> {
+    return this.http.get<RESTCountries[]>(`${this.baseUrl}/all`);
+  }
+}
