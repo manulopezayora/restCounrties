66b53453db0013bb57d02fc6d6df38eeeb6654b9
Manuel Jesús López <51477144+manulopezayora@users.noreply.github.com>
2022-10-21 19:44:51 +0200

    [DEV] Add dark mode

--- src/app/home/shared/header/header.component.ts
+++ src/app/home/shared/header/header.component.ts
@@ -7,16 +7,43 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
 })
 export class HeaderComponent implements OnInit {
 
-  @Input() darkMode: boolean;
+  public isDarkMode: boolean;
 
   constructor() {
-    this.darkMode = false;
+    this.isDarkMode = false;
   }
 
   ngOnInit(): void {
   }
 
   public changeDarkMode(): void {
-    this.darkMode = !this.darkMode;
+    this.isDarkMode = !this.isDarkMode;
+    this.setColorTheme(this.isDarkMode);
+  }
+
+  private setColorTheme(isDarkMode: boolean) {
+    const lightTheme = {
+      "--card-background": "rgba(255, 255, 255, 0.9)",
+      "--font-color": "rgb(17, 21, 23)",
+      "--main-background": "rgb(250, 250, 250)",
+      "--card-box-shadow": "0 5px 10px 0 rgba(86, 87, 92, 0.37)"
+    }
+    
+    const darkTheme = {
+      "--card-background": "rgb(43, 57, 69)",
+      "--font-color": "rgb(255, 255, 255)",
+      "--main-background": "rgb(32, 44, 55)",
+      "--card-box-shadow": "none"
+    }
+    isDarkMode ? this.changeTheme(darkTheme) : this.changeTheme(lightTheme);
+  }
+
+  private changeTheme(theme: any) {
+    const styles = document.documentElement.style;
+    const customStyles = Object.keys(theme);
+    for (const style of customStyles) {
+      styles.setProperty(style, theme[style])
+    }
   }
 }
+
