a8463dd59506f87152fd91ab6a551852bc45c02a
Manuel Jesús López <51477144+manulopezayora@users.noreply.github.com>
2022-10-21 18:02:41 +0200

    [DEV] Add search functionality

--- src/app/home/pages/country-list/country-list.component.ts
+++ src/app/home/pages/country-list/country-list.component.ts
@@ -11,25 +11,29 @@ import { RestCountriesService } from '../../services/restCountries/rest-countrie
 export class CountryListComponent implements OnInit {
 
   public countriesList: RESTCountries[];
+  private _allCountriesList: RESTCountries[];
 
-  constructor(private restCountriesService: RestCountriesService) {
+  constructor(private _restCountriesService: RestCountriesService) {
     this.countriesList = [];
+    this._allCountriesList = [];
   }
 
   ngOnInit(): void {
-    this.initialize();
+    this._initialize();
   }
 
   public searchCountry(event: any) {
+    this.countriesList = this._allCountriesList.filter((element) => element.name.toLowerCase().includes(event));
     console.log(event)
   }
 
-  private async initialize(): Promise<void> {
-    this.countriesList = await this.getAllCountries();
+  private async _initialize(): Promise<void> {
+    this._allCountriesList = await this._getAllCountries();
+    this.countriesList = [...this._allCountriesList];
   }
 
-  private async getAllCountries(): Promise<RESTCountries[]> {
-    const source$ = this.restCountriesService.getAllCountries().pipe(take(1));
+  private async _getAllCountries(): Promise<RESTCountries[]> {
+    const source$ = this._restCountriesService.getAllCountries().pipe(take(1));
     const allCountries: RESTCountries[] = await lastValueFrom(source$);
     return allCountries;
   }
