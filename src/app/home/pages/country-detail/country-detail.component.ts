ae270c841e80aef76a5634524fbcb3ec6aa8cd12
Manuel Jesús López <51477144+manulopezayora@users.noreply.github.com>
2023-01-08 20:41:29 +0100

    [FIX] Style and back button functionality

--- src/app/home/pages/country-detail/country-detail.component.ts
+++ src/app/home/pages/country-detail/country-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
 import { lastValueFrom, take } from 'rxjs';
 import { RESTCountries } from '../../interfaces/RESTCountries.interface';
 import { RestCountriesService } from '../../services/restCountries/rest-countries.service';
@@ -7,23 +8,27 @@ import { RestCountriesService } from '../../services/restCountries/rest-countrie
 @Component({
   selector: 'app-country-detail',
   templateUrl: './country-detail.component.html',
-  styleUrls: ['./country-detail.component.scss']
+  styleUrls: ['./country-detail.component.scss'],
 })
 export class CountryDetailComponent implements OnInit {
-
   public countryDetail!: RESTCountries;
   private _allCountries!: RESTCountries[];
 
   constructor(
     private _restCountriesService: RestCountriesService,
     private _route: ActivatedRoute,
-    private _router: Router
-    ) {
-  }
+    private _router: Router,
+    private _location: Location
+  ) {}
 
   ngOnInit(): void {
-    const alpha3Code: string = this._route.snapshot.paramMap.get('alpha3Code') || '';
+    const alpha3Code: string =
+      this._route.snapshot.paramMap.get('alpha3Code') || '';
     this._initialize(alpha3Code);
+  }
+
+  public backTo(): void {
+    this._location.back();
   }
 
   public onClickBorderButton(country: RESTCountries): void {
@@ -31,19 +36,20 @@ export class CountryDetailComponent implements OnInit {
     this._initialize(country.alpha3Code);
   }
 
-
   private async _initialize(alpha3Code: string): Promise<void> {
     this._allCountries = await this._getAllCountries();
     this.countryDetail = await this._getCountryDetail(alpha3Code);
   }
 
   private async _getCountryDetail(alpha3Code: string): Promise<RESTCountries> {
-    const source$ = this._restCountriesService.getCountryDetail(alpha3Code).pipe(take(1));
+    const source$ = this._restCountriesService
+      .getCountryDetail(alpha3Code)
+      .pipe(take(1));
     const countryDetail: RESTCountries = await lastValueFrom(source$);
     return {
       ...countryDetail,
-      bordersBtn: await this.setDetail(countryDetail)
-    }
+      bordersBtn: await this.setDetail(countryDetail),
+    };
   }
 
   private async _getAllCountries(): Promise<RESTCountries[]> {
@@ -55,7 +61,7 @@ export class CountryDetailComponent implements OnInit {
   private async setDetail(countryDetail: RESTCountries) {
     let borders: any[] = [];
     countryDetail.borders?.forEach((el) => {
-      const borderCountry = this._allCountries.find(c => c.alpha3Code === el);
+      const borderCountry = this._allCountries.find((c) => c.alpha3Code === el);
       borders.push(borderCountry);
     });
     return borders;
